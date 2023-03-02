25f80dc7c5cfe62eb45e2b6e369d318df3f3d89c
upgrace-in <thedesiretree47@gmail.com>
2023-03-02 11:12:42 +0530

    work

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- todo.md
+++ todo.md
@@ -1,1 +1,8 @@
 # I AM THERE - (CHATGPT Project)
+
+# Steps
+
+    1. UI (input/output design)
+    2. Speech to Text handler
+    3. API point to send input to CHATGPT and return output
+    4. Text to Speech handler
